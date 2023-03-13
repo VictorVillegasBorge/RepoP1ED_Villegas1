54cd66896bfb513b10676973992560494053f150
Victor <Victor@DESKTOP-S1L2ECO>
2023-03-13 09:29:09 +0100

    Tercera modificación del código.

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		System.out.println("Programa que nos permitirá hacer un control de versiones con Git."
-				+ "Segunda modificación");
+				+ "Tercera modificación");
 	}
 }
 
