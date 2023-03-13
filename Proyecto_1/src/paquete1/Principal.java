1be38443fbe503f792e917a42c0dce4bb0b42a08
Victor <Victor@DESKTOP-S1L2ECO>
2023-03-13 09:22:11 +0100

    Segunda modificación

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -3,7 +3,8 @@ package paquete1;
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Programa que nos permitirá hacer un control de versiones con Git");
+		System.out.println("Programa que nos permitirá hacer un control de versiones con Git."
+				+ "Segunda modificación");
 	}
 }
 
