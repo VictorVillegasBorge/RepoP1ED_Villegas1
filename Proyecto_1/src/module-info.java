dd5fa3edbbc6014cf88135b935b1eae1168afddb
Victor <Victor@DESKTOP-S1L2ECO>
2023-03-13 09:17:16 +0100

    Primera versión programa control versiones.

---
+++ Proyecto_1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Victor
+ *
+ */
+module Proyecto_1 {
+}
---
+++ Proyecto_1/src/paquete1/Principal.java
@@ -0,0 +1,9 @@
+package paquete1;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		System.out.println("Programa que nos permitirá hacer un control de versiones con Git");
+	}
+}
+
