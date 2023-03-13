f75f30ff7f974d234e967b3b27b6e41f7db307ec
Victor <Victor@DESKTOP-S1L2ECO>
2023-03-13 10:48:39 +0100

    Código mensaje por pantalla creado para rama 1 del repositorio.

---
+++ Proyecto_1/src/paqueteRama1/Rama1.java
@@ -0,0 +1,8 @@
+package paqueteRama1;
+
+public class Rama1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Este codigo pertenece a la rama 1 ");
+	}
+}
---
+++ Proyecto_1/src/paqueteRama1/package-info.java
@@ -0,0 +1,1 @@
+package paqueteRama1;
